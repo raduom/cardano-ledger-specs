c348736c14433483d3a2169263b1382ab3c71fd7
Polina Vinogradova <polina.vino@gmail.com>
2020-03-19 15:35:35 -0400

    fix rebase stuff

--- shelley/chain-and-ledger/executable-spec/src/Shelley/Spec/Ledger/UTxO.hs
+++ shelley/chain-and-ledger/executable-spec/src/Shelley/Spec/Ledger/UTxO.hs
@@ -244,7 +244,7 @@ txinsScript
   -> Set (TxIn crypto)
 txinsScript txInps (UTxO u) =
   txInps `Set.intersection`
-  Map.keysSet (Map.filter (\(UTxOOut a _) ->
-                               case a of
+  Map.keysSet (Map.filter (\utxoout ->
+                               case getAddress utxoout of
                                  Addr (ScriptHashObj _) _     -> True
                                  _                                -> False) u)
