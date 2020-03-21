33cc4787dce7449a54df3431eaedc9744ee10919
Polina Vinogradova <polina.vino@gmail.com>
2020-03-21 19:23:53 -0400

    working on Utxow

--- shelley/chain-and-ledger/executable-spec/src/Shelley/Spec/Ledger/STS/Utxo.hs
+++ shelley/chain-and-ledger/executable-spec/src/Shelley/Spec/Ledger/STS/Utxo.hs
@@ -163,13 +163,13 @@ utxoInductive = do
   txins txb ⊆ dom utxo ?! BadInputsUTxO
 
   let consumed_ = consumed pp utxo stakeCreds txb
-      produced_ = produced pp stakepools txb
+      produced_ = produced slot pp stakepools txb
   consumed_ == produced_ ?! ValueNotConservedUTxO (toValBST consumed_) (toValBST produced_)
 
   -- process Update Proposals
   ups' <- trans @(UP crypto) $ TRC (UpdateEnv slot pp genDelegs, ups, txup tx)
 
-  let outputValues = [getValue utxoout | utxoout <- Set.toList (range (txouts txb))]
+  let outputValues = [getValue utxoout | utxoout <- Set.toList (range (txouts slot txb))]
   all (zeroV <=) outputValues ?! NegativeOutputsUTxO
 
   let (Value vls) = _forge txb
